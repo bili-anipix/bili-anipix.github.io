cdd252e0cf697aee51dab01c2e0d819e7ee76474
RyuSeiri <56245152+RyuSeiri@users.noreply.github.com>
2026-02-03 20:28:22 +0900

    update

--- components/image.tsx
+++ components/image.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 
 interface ImageProps {
   alt?: string;
@@ -10,27 +10,42 @@ interface ImageProps {
 }
 
 export function CustomImage({ alt, src, title, className, sizes }: ImageProps) {
-  const ref = useRef(null);
-  useEffect(() => {
-    const imgContainer: any = ref.current;
-    let img: any = imgContainer.querySelector("img");
-    if (!img) {
-      img = new Image();
-    }
-    img.src = "https://i0.hdslb.com/bfs/new_dyn/245cfc65f32d6b057ce9980378f330ef516035062.jpg";
-    // img.onerror = (e: any) => {
-    //   img.src = "/placeholder.svg";
-    //   console.log("Image not found", e);
-    // };
-    img.alt = alt;
-    img.title = title || alt;
-    img.loading = "lazy";
-    img.decoding = "async";
-    img.className = className;
-    img.style = sizes;
-    img.dataset.nimg="fill";
-    imgContainer.appendChild(img);
-  }, [src]);
+  // const ref = useRef(null);
+  // useEffect(() => {
+  //   const imgContainer: any = ref.current;
+  //   let alink: any = imgContainer.querySelector("a");
+  //   let img: any = imgContainer.querySelector("img");
+  //   if (!alink) {
+  //     alink = document.createElement("a");
+  //   }
+  //   if (!img) {
+  //     img = new Image();
+  //   }
 
-  return <div ref={ref}></div>;
+  //   alink.href =
+  //     "https://i0.hdslb.com/bfs/new_dyn/245cfc65f32d6b057ce9980378f330ef516035062.jpg";
+  //   alink.target = "_blank";
+  //   alink.rel = "noopener noreferrer nofollow";
+  //   img.src =
+  //     "https://i0.hdslb.com/bfs/new_dyn/245cfc65f32d6b057ce9980378f330ef516035062.jpg";
+  //   // img.onerror = (e: any) => {
+  //   //   img.src = "/placeholder.svg";
+  //   //   console.log("Image not found", e);
+  //   // };
+  //   img.alt = alt;
+  //   img.title = title || alt;
+  //   img.loading = "lazy";
+  //   img.decoding = "async";
+  //   img.className = className;
+  //   img.style = sizes;
+  //   img.dataset.nimg = "fill";
+  //   alink.appendChild(img);
+  //   imgContainer.appendChild(alink);
+  // }, [src]);
+
+  return (
+    <div >
+      <img src={`/api/bili-img?url=${src}`} alt={alt} className={className} />
+    </div>
+  );
 }
